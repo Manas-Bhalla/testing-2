34d7eda3ead05a0a8942e4ff01c7e94accfb69b3
Manas-Bhalla <manasbhalla1983@gmail.com>
2025-01-29 21:11:41 -0800

    Add keyboard controls for snake movement using WASD keys

--- GamePanel.java
+++ GamePanel.java
@@ -142,21 +142,25 @@ public class GamePanel extends JPanel implements ActionListener{
         public void keyPressed(KeyEvent e){
             switch (e.getKeyCode()){
                 case KeyEvent.VK_LEFT:
+                case KeyEvent.VK_A:
                     if (direction != 'R'){
                         direction = 'L';
                     }
                     break;
                 case KeyEvent.VK_RIGHT:
+                case KeyEvent.VK_D:
                     if (direction != 'L'){
                         direction = 'R';
                     }
                     break;
                 case KeyEvent.VK_UP:
+                case KeyEvent.VK_W:
                     if (direction != 'D'){
                         direction = 'U';
                     }
                     break;
                 case KeyEvent.VK_DOWN:
+                case KeyEvent.VK_S:
                     if (direction != 'U'){
                         direction = 'D';
                     }
